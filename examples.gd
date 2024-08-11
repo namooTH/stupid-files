4dd028c8c94552aee703bbad7ea08ab2a13b35f1
Namu <73419369+namooTH@users.noreply.github.com>
2024-08-11 13:53:04 +0700

    Update examples.gd

--- examples.gd
+++ examples.gd
@@ -1,6 +1,9 @@
 extends Node
 
 func _ready() -> void:
+	# creates a catagory called "hello" with the item called "world" with a value true
 	StupidFiles.store("hello", "world", true)
+	# prints the value
 	print(StupidFiles.get_data("hello", "world"))
+	# deletes the item
 	StupidFiles.delete("hello", "world")
